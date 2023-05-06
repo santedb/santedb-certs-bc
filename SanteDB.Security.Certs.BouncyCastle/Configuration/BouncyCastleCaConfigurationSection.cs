ce0d42b76d728372c59c6df3e6cfed57fd0c8e43
Justin Fyfe <justin@fyfesoftware.ca>
2023-05-06 18:50:04 -0400

    Updated the CA configuration section for an eventual storage of CSR and CRL in database

---
+++ SanteDB.Security.Certs.BouncyCastle/Configuration/BouncyCastleCaConfigurationSection.cs
@@ -0,0 +1,16 @@
+﻿using SanteDB.OrmLite.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SanteDB.Security.Certs.BouncyCastle.Configuration
+{
+    /// <summary>
+    /// Bouncy castle CA configuration
+    /// </summary>
+    [XmlType(nameof(BouncyCastleCaConfigurationSection), Namespace = "http://santedb.org/configuration")]
+    public class BouncyCastleCaConfigurationSection : OrmConfigurationBase
+    {
+    }
+}
