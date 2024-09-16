91a62b9c112a8f0d07b6c6e1ccdb90123dab8a17
Justin Fyfe <justin@fyfesoftware.ca>
2024-09-16 10:14:18 -0400

    Updated the license headers to remove user/date of file (they were not accurate - as using file system times)

--- SanteDB.Security.Certs.BouncyCastle/BouncyCastleCertificateSigner.cs
+++ SanteDB.Security.Certs.BouncyCastle/BouncyCastleCertificateSigner.cs
@@ -15,8 +15,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  *
- * User: fyfej
- * Date: 2023-6-21
  */
 using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Security;
